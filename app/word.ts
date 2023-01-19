53d30798269d52f136102095810e8e40b96895f6
Charles Myerov <114505748+CornJuice14@users.noreply.github.com>
2023-01-19 15:35:35 +0000

    removed spaces and changed instructions

--- app/word.ts
+++ app/word.ts
@@ -1,7 +1,7 @@
 import {letterManager} from './letters';
 let x = 10
 let y = -300
-for (let word of ["hello world","cj rocks", "good job", "javascript", "having fun", "keep playing", "you are so close", "boss is next", "supercalifragilisticexpialidocious"]) {
+for (let word of ["helloworld","cjrocks", "goodjob", "javascript", "havingfun", "keepplaying", "youaresoclose", "bossisnext", "supercalifragilisticexpialidocious"]) {
     y -= 300
     x = 0
   for (let ltr of word) {
